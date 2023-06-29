d57a6ed4978432b552ac1a8df826a23b6b7cd454
Ram-SrinivasChandran <83490721+Ram-SrinivasChandran@users.noreply.github.com>
2023-06-29 14:56:15 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,31 @@
+const products=[{
+  id:'id1',
+  image:'Product-images/athletic-cotton-socks-6-pairs.jpg',
+  name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating:{
+    stars:'ratings/rating-45.png',
+    count:87
+  },
+  costIncent:1090
+},
+{
+  id:'id2',
+  image:'Product-images/intermediate-composite-basketball.jpg',
+  name:'Intermediate Size Basketball',
+  rating:{
+    stars:'ratings/rating-40.png',
+    count:127
+  },
+  costIncent:2095
+},
+{
+  id:'id3',
+  image:'Product-images/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name:'Adult Plain Cotton T-Shirt - 2 Pairs',
+  rating:{
+    stars:'ratings/rating-45.png',
+    count:56
+  },
+  costIncent:799
+},
+];
